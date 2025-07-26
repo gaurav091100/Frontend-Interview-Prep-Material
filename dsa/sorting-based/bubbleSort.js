b89b61758e526146fa93d2a01603f590abb955d5
gaurav091100 <gaurav091100@gmail.com>
2025-07-26 17:12:32 +0530

    added beginner levels dsa questions for string, arrays and sorting

---
+++ dsa/sorting-based/bubbleSort.js
@@ -0,0 +1,14 @@
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSort([10,2,3,5,19,23,10,23,45,255]))
